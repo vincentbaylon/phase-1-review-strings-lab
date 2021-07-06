88285d418a4ae81f30aa1f53b9fcfb5af49ea0c0
vincentbaylon <vincentbaylon@gmail.com>
2021-07-05 20:58:39 -0700

    commit tree 9c28c135395ba2abf45423bb2a7f02840feb8441

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Roscoe Baylon';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
